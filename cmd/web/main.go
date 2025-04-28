fdb3c71a54ed02ddff39ae5079acc2e6dc6ba764
Alex Perea <ing.alexperea@gmail.com>
2025-04-28 07:00:31 -0500

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/alexpereap/go-bookings/pkg/config"
+	"github.com/alexpereap/go-bookings/pkg/handlers"
+	"github.com/alexpereap/go-bookings/pkg/render"
+)
+
+const portNumber string = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main app function
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Can't create template cache", err)
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Printf("Starting application on port %s", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+#Bookings and Reservations
+
+Repo for bookings reservations project
+
+- Built in go 1.22.2
+- Uses [chi router](https://github.com/go-chi/chi)
+- Uses [alex edwards SCS](https://github.com/alexedwards/scs)
+- Uses [nosurf](https://github.com/justinas/nosurf)
